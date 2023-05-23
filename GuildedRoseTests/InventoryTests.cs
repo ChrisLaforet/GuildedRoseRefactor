ba243e15a9f2fb53d59a2b17624d3e6d7603f3cd
chris <laforet@chrislaforetsoftware.com>
2023-05-22 20:48:06 -0400

    First refactoring steps

---
+++ GuildedRoseTests/InventoryTests.cs
@@ -0,0 +1,22 @@
+﻿using GildedRose;
+
+namespace GuildedRoseTests;
+
+public class InventoryTests
+{
+    [Fact]
+    public void GivenNothing_WhenCreatingInventory_ThenInventoryContainsItems()
+    {
+        var inventory = new Inventory();
+        Assert.True(inventory.Items.Count > 0);
+    }
+
+    [Fact]
+    public void GivenInventory_WhenOneUpdateQuality_ThenElixirOfTheMongooseQualityAges()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Elixir of the Mongoose");
+        inventory.UpdateQuality();
+        Assert.Equal(6, item.Quality);
+    }
+}
