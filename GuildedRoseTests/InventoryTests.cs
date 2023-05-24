044c4b9f1598d99e380efb17fed69998e3105f11
chris <laforet@chrislaforetsoftware.com>
2023-05-24 10:17:38 -0400

    Adding additional stabilizing tests to anchor to the rules

--- GuildedRoseTests/InventoryTests.cs
+++ GuildedRoseTests/InventoryTests.cs
@@ -12,7 +12,7 @@ public class InventoryTests
     }
 
     [Fact]
-    public void GivenInventory_WhenOneUpdateQuality_ThenElixirOfTheMongooseQualityAges()
+    public void GivenInventory_WhenUpdateQuality_ThenElixirOfTheMongooseQualityAges()
     {
         var inventory = new Inventory();
         var item = inventory.FindItem("Elixir of the Mongoose");
@@ -27,6 +27,27 @@ public class InventoryTests
         var inventory = new Inventory();
         var item = inventory.FindItem("Sulfuras, Hand of Ragnaros");
         var initialQuality = item.Quality;
+        inventory.UpdateQuality();
+        Assert.Equal(initialQuality, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQuality_ThenSulfurasSellInDoesNotChange()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Sulfuras, Hand of Ragnaros");
+        var initialSellIn = item.SellIn;
+        inventory.UpdateQuality();
+        Assert.Equal(initialSellIn, item.SellIn);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQualityAtExpiredSellIn_ThenSulfurasDoesNotAge()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Sulfuras, Hand of Ragnaros");
+        var initialQuality = item.Quality;
+        item.SellIn = 0;
         inventory.UpdateQuality();
         Assert.Equal(initialQuality, item.Quality);
     }
@@ -69,9 +90,31 @@ public class InventoryTests
         var inventory = new Inventory();
         var item = inventory.FindItem("Backstage passes to a TAFKAL80ETC concert");
         var initialQuality = item.Quality;
-        item.SellIn = 9;
+        item.SellIn = 10;
         inventory.UpdateQuality();
         Assert.Equal(initialQuality + 2, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQualityWithSellInLessThan5_ThenBackstagePassesQualityIncreasesBy3()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Backstage passes to a TAFKAL80ETC concert");
+        var initialQuality = item.Quality;
+        item.SellIn = 4;
+        inventory.UpdateQuality();
+        Assert.Equal(initialQuality + 3, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQualityWithSellIs5_ThenBackstagePassesQualityIncreasesBy3()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Backstage passes to a TAFKAL80ETC concert");
+        var initialQuality = item.Quality;
+        item.SellIn = 5;
+        inventory.UpdateQuality();
+        Assert.Equal(initialQuality + 3, item.Quality);
     }
     
     [Fact]
@@ -82,5 +125,36 @@ public class InventoryTests
         item.SellIn = 0;
         inventory.UpdateQuality();
         Assert.Equal(0, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQualityOf50_ThenAgedBrieQualityDoesNotIncrease()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Aged Brie");
+        item.Quality = 50;
+        inventory.UpdateQuality();
+        Assert.Equal(50, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenQualityIsZero_ThenElixirOfTheMongooseQualityDoesNotBecomeNegative()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Elixir of the Mongoose");
+        item.Quality = 0;
+        inventory.UpdateQuality();
+        Assert.Equal(0, item.Quality);
+    }
+    
+    [Fact]
+    public void GivenInventory_WhenUpdateQualityPastSellInDate_ThenElixirOfTheMongooseQualityAgesTwiceAsFast()
+    {
+        var inventory = new Inventory();
+        var item = inventory.FindItem("Elixir of the Mongoose");
+        var initialQuality = item.Quality;
+        item.SellIn = 0;
+        inventory.UpdateQuality();
+        Assert.Equal(initialQuality - 2, item.Quality);
     }
 }
